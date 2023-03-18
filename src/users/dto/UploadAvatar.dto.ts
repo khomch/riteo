be7e8ec9886e6de817fd39f33d7a279b47243665
Anton Khomchenko <khomchenko.anton@yandex.ru>
2023-03-18 11:57:05 +0000

    [FEAT] avatar upload added

--- src/users/dto/UploadAvatar.dto.ts
+++ src/users/dto/UploadAvatar.dto.ts
@@ -1,13 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Column, DataType } from 'sequelize-typescript';
-import { IsNumber } from 'class-validator';
 
 export class UploadAvatarDto {
 
   @ApiProperty({ example: "77", description: "User ID" })
   readonly userId: number;
 
-  // @ApiProperty({ example: "77", description: "User ID" })
-  // @IsNumber({}, { message: "Should be a number" })
-  // readonly userId: number;
+  @ApiProperty({ example: "avatar.jpg", description: "User Avatar" })
+  readonly avatar: string;
+
 }
